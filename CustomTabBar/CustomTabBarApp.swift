5b92df2c7e1f548a448f4a9c4a66ec2fb3880fc4
Roman-Fedotov-iOS <romafed23@gmail.com>
2021-08-03 21:38:00 +0300

    Initial Commit

---
+++ CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by Roman Fedotov on 03.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
